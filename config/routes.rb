69f84d4414dd195709d140e5c79d0527aa970458
Laurent Binder <laurentbinder@gmail.com>
2020-11-17 21:14:35 +0100

    routes locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  scope ':locale', locale: /fr|es/ do
+
   root to: 'pages#home'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
